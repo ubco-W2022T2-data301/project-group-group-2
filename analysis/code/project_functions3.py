504930f41f667ebc1b52a1e35cba493f2a1f76e9
TheUQGuy <90714695+TheUQGuy@users.noreply.github.com>
2023-03-26 21:13:56 -0700

    Git finished Milestone 4 Stephen

---
+++ analysis/code/project_functions3.py
@@ -0,0 +1,22 @@
+import seaborn as sns
+import numpy as np
+import pandas as pd
+def load_and_process(url):
+    df = (pd.read_csv(url)
+    .dropna(subset="VALUE")
+      .rename(columns={'GEO':'LOCATION', 'REF_DATE':'DATE', 'New housing price indexes': 'House/land'})
+    .drop(columns=["DGUID", "UOM", "UOM_ID", "SCALAR_ID","SCALAR_FACTOR", "COORDINATE", 'TERMINATED', 'SYMBOL', 'STATUS', 'DECIMALS', 'VECTOR'])
+     .loc[lambda x: x['House/land'].str.contains('House only')]
+      .drop(columns=['House/land'])
+      .assign(St_JOHNS = lambda x: x.VALUE.where(x.LOCATION.str.contains("St. John's")), 
+              QUEBEC = lambda x: x.VALUE.where(x.LOCATION.str.contains("Quebec")),
+              OTTAWA = lambda x: x.VALUE.where(x.LOCATION.str.contains("Ottawa")),
+              VICTORIA = lambda x: x.VALUE.where(x.LOCATION.str.contains("Victoria")),
+              REGINA = lambda x: x.VALUE.where(x.LOCATION.str.contains("Regina")),
+              EDMONTON = lambda x: x.VALUE.where(x.LOCATION.str.contains("Edmonton")),
+              HALIFAX = lambda x: x.VALUE.where(x.LOCATION.str.contains("Halifax"))
+             )
+      .drop(columns='VALUE')
+      .groupby('DATE')[['St_JOHNS', 'QUEBEC', 'OTTAWA', 'VICTORIA', 'REGINA', 'EDMONTON', 'HALIFAX']].first().reset_index()
+     )
+    return df
