70ade38fafa6977e67f35eb9255e9a19049d1991
Jack Saunders <jacksaunders1213@gmail.com>
2023-04-06 16:24:37 -0700

    Minor Updates to .py #2

--- analysis/code/project_function2.py
+++ analysis/code/project_function2.py
@@ -12,3 +12,40 @@ def load_and_process(url):
     df['DATE'] = pd.to_datetime(df['DATE'])
     df['MONTH'] = df['DATE'].dt.month
     return df
+
+def value_mean_progression():
+     df = (pd.read_csv(url)
+          .rename(columns={'GEO':'PLACE', 'REF_DATE':'DATE', 'New housing price indexes': 'PLOT'})
+          .drop(columns=["DGUID", "UOM", "UOM_ID", "SCALAR_ID","SCALAR_FACTOR", "COORDINATE", 'TERMINATED', 'SYMBOL', 'STATUS', 'DECIMALS', 'VECTOR'])
+         )
+    df = df[df['PLOT'] == "Total (house and land)"]
+    df['DATE'] = pd.to_datetime(df['DATE'])
+    df['MONTH'] = df['DATE'].dt.month
+    df.groupby(["MONTH","VALUE"]).agg('mean','count')
+    return df
+
+def value_max_progression():
+     df = (pd.read_csv(url)
+          .rename(columns={'GEO':'PLACE', 'REF_DATE':'DATE', 'New housing price indexes': 'PLOT'})
+          .drop(columns=["DGUID", "UOM", "UOM_ID", "SCALAR_ID","SCALAR_FACTOR", "COORDINATE", 'TERMINATED', 'SYMBOL', 'STATUS', 'DECIMALS', 'VECTOR'])
+         )
+    df = df[df['PLOT'] == "Total (house and land)"]
+    df['DATE'] = pd.to_datetime(df['DATE'])
+    df['MONTH'] = df['DATE'].dt.month
+    df.groupby('MONTH')['VALUE'].max().reset_index()
+    return df
+
+
+
+def value_max_place():
+     df = (pd.read_csv(url)
+          .rename(columns={'GEO':'PLACE', 'REF_DATE':'DATE', 'New housing price indexes': 'PLOT'})
+          .drop(columns=["DGUID", "UOM", "UOM_ID", "SCALAR_ID","SCALAR_FACTOR", "COORDINATE", 'TERMINATED', 'SYMBOL', 'STATUS', 'DECIMALS', 'VECTOR'])
+         )
+    df = df[df['PLOT'] == "Total (house and land)"]
+    df['DATE'] = pd.to_datetime(df['DATE'])
+    df['MONTH'] = df['DATE'].dt.month
+    df.groupby('PLACE')['VALUE'].max().reset_index()
+    return df
+
+
